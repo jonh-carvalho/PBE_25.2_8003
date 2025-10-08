2e51716bc61405acfe08f531520c459cf5f13114
Jonh Carvalho <jonh.carvalho@professores.ibmec.edu.br>
2025-10-08 11:42:49 -0300

    update src

---
+++ src/Intro/myapp/api_urls.py
@@ -0,0 +1,11 @@
+# myapp/api_urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from myapp.api import ProdutoViewSet
+
+router = DefaultRouter()
+router.register(r'produtos', ProdutoViewSet, basename='produto')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
